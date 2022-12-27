e1beb9c3e293395a7fdef4868f911fe8cc9439ca
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-12-27 18:20:31 -0500

    Update formatResults.vb

--- src/formatResults.vb
+++ src/formatResults.vb
@@ -2,31 +2,35 @@ Sub formatResults()
 
   ' Define Range and conditions
   Dim textRng As Range
-  Dim winCondition As FormatCondition, lossCondition As FormatCondition
-  
-  'Fixing/Setting the range on which conditional formatting is to be desired
+
+  ' n times for colItr to repeat
+  Dim colNum as Long
+  ' hack: not dynamic
+  colNum = 108
+
+  ' Setting the STARTING range on which conditional formatting is to be desired
   Set textRng = Worksheets("view").Range("D2:D43")
 
-  'To delete/clear any existing conditional formatting from the range
+  ' To delete/clear any existing conditional formatting from the range
   textRng.FormatConditions.Delete
 
-  'Create variables to hold the number of rows for the tabular data
-  Dim RRow As Long, N As Long
-
-  'Capture the number of rows within the tabular data range
-  RRow = textRng.Rows.Count
+  ' number of rows for the tabular data
+  Dim rowNum As Long
   
-  'Iterate through all the rows in the tabular data range
-  For N = 1 To RRow
-    'Use a Select Case statement to evaluate the formatting based on column 2
-    Select Case textRng.Cells(N, 2).Value
+  ' Capture the number of rows within the tabular data range
+  rowNum = textRng.Rows.Count
+  
+  ' Iterate through all the rows in the tabular data range
+  For rowItr = 1 To rowNum
+    ' Use a Select Case statement to evaluate the formatting based on column 2
+    Select Case textRng.Cells(rowItr, 2).Value
       Case "TBD."
-        textRng.Cells(N, 1).Font.Color = RGB(0,0,0)
-        textRng.Cells(N, 1).Interior.Color = RGB(200,200,200)
+        textRng.Cells(rowItr, 1).Font.Color = RGB(0,0,0)
+        textRng.Cells(rowItr, 1).Interior.Color = RGB(200,200,200)
       Case 0
-        textRng.Cells(N, 1).Interior.Color = RGB(255,0,0)
+        textRng.Cells(rowItr, 1).Interior.Color = RGB(255,0,0)
       Case > 0
-        textRng.Cells(N, 1).Interior.Color = RGB(146,208,80)
+        textRng.Cells(rowItr, 1).Interior.Color = RGB(146,208,80)
     End Select
-  Next N
+  Next rowItr
 End Sub
