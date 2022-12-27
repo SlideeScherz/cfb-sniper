f00e05f18ef7a0f71c22a7c93a7e25d344db9798
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-12-27 18:03:41 -0500

    rn and colors

--- src/formatResults.vb
+++ src/formatResults.vb
@@ -1,5 +1,4 @@
-
-Sub formatWinners()
+Sub formatResults()
 
   ' Define Range and conditions
   Dim textRng As Range
@@ -19,19 +18,14 @@ Sub formatWinners()
   
   'Iterate through all the rows in the tabular data range
   For N = 1 To RRow
-      'Use a Select Case statement to evaluate the formatting based on column 2
-      Select Case textRng.Cells(N, 2).Value
-          'Turn the interior color to blue
-          Case 0
-          textRng.Cells(N, 1).Interior.Color = vbBlue
-          'Turn the interior color to red
-          Case > 0
-          textRng.Cells(N, 1).Interior.Color = vbRed
-          'Turn the interior color to green
-          Case "TBD."
-          textRng.Cells(N, 1).Interior.Color = vbGreen
-      End Select
+    'Use a Select Case statement to evaluate the formatting based on column 2
+    Select Case textRng.Cells(N, 2).Value
+      Case "TBD."
+        textRng.Cells(N, 1).Font.Color = vbGray
+      Case 0
+        textRng.Cells(N, 1).Interior.Color = RGB(255,0,0)
+      Case > 0
+        textRng.Cells(N, 1).Interior.Color = RGB(146,208,80)
+    End Select
   Next N
-
 End Sub
-
