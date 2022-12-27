a1951cd890f3bdf1b749b40b6648c9710b593277
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-12-27 18:26:41 -0500

    All color formatting pass

--- src/formatResults.vb
+++ src/formatResults.vb
@@ -3,7 +3,7 @@ Sub formatResults()
   ' Define Range and conditions
   Dim textRng As Range
 
-  ' n times for colItr to repeat
+  ' number of columns for the tabular data
   Dim colNum as Long
   ' hack: not dynamic
   colNum = 108
@@ -11,26 +11,31 @@ Sub formatResults()
   ' Setting the STARTING range on which conditional formatting is to be desired
   Set textRng = Worksheets("view").Range("D2:D43")
 
-  ' To delete/clear any existing conditional formatting from the range
-  textRng.FormatConditions.Delete
+ ' Iterate through all the rows in the tabular data range
+  For colItr = 1 To colNum Step 2 
 
-  ' number of rows for the tabular data
-  Dim rowNum As Long
-  
-  ' Capture the number of rows within the tabular data range
-  rowNum = textRng.Rows.Count
-  
-  ' Iterate through all the rows in the tabular data range
-  For rowItr = 1 To rowNum
-    ' Use a Select Case statement to evaluate the formatting based on column 2
-    Select Case textRng.Cells(rowItr, 2).Value
-      Case "TBD."
-        textRng.Cells(rowItr, 1).Font.Color = RGB(0,0,0)
-        textRng.Cells(rowItr, 1).Interior.Color = RGB(200,200,200)
-      Case 0
-        textRng.Cells(rowItr, 1).Interior.Color = RGB(255,0,0)
-      Case > 0
-        textRng.Cells(rowItr, 1).Interior.Color = RGB(146,208,80)
-    End Select
-  Next rowItr
+    ' To delete/clear any existing conditional formatting from the range
+    textRng.FormatConditions.Delete
+
+    ' number of rows for the tabular data
+    Dim rowNum As Long
+    
+    ' Capture the number of rows within the tabular data range
+    rowNum = textRng.Rows.Count
+    
+    ' Iterate through all the rows in the tabular data range
+    For rowItr = 1 To rowNum
+      ' Use a Select Case statement to evaluate the formatting based on column 2
+      Select Case textRng.Cells(rowItr, colItr + 1).Value
+        Case "TBD."
+          textRng.Cells(rowItr, colItr).Font.Color = RGB(0,0,0)
+          textRng.Cells(rowItr, colItr).Interior.Color = RGB(200,200,200)
+        Case 0
+          textRng.Cells(rowItr, colItr).Interior.Color = RGB(255,0,0)
+        Case > 0
+          textRng.Cells(rowItr, colItr).Interior.Color = RGB(146,208,80)
+      End Select
+    Next rowItr
+
+  Next colItr
 End Sub
