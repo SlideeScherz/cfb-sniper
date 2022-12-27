dc43684e236428098c6b9eeaadbcbbb6992df8f9
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-12-27 16:57:27 -0500

    init format

---
+++ src/formatWinners.vb
@@ -0,0 +1,29 @@
+
+Sub formatWinners()
+
+  ' Define Range and conditions
+  Dim textRng As Range, ptsRng As Range
+  Dim winCondition As FormatCondition, lossCondition As FormatCondition
+
+  'Fixing/Setting the range on which conditional formatting is to be desired
+  Set textRng = Worksheets("view").Range("D3:D43")
+
+  'To delete/clear any existing conditional formatting from the range
+  textRng.FormatConditions.Delete
+
+  'Defining and setting the criteria for each conditional format
+  Set winCondition = textRng.FormatConditions.Add(xlCellValue, xlGreater, "=0")
+  Set lossCondition = textRng.FormatConditions.Add(xlCellValue, xlEqual, "=0")
+
+  'Defining and setting the format to be applied for each condition
+  With winCondition
+    .Font.Color = vbGreen
+    .Font.Bold = False
+  End With
+
+  With lossCondition
+    .Font.Color = vbRed
+    .Font.Bold = False
+  End With
+
+End Sub
